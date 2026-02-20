c53707c6cb70938c66d1aea10a443d0c35dea375
madhumarvel123 <madhukumargopal1999@gmail.com>
2026-02-19 18:50:12 -0600

    Updated changes (removed large file)

--- app1.py
+++ app1.py
@@ -1,9 +1,12 @@
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+import shutil   
+from fastapi import Form
+
 
 app = FastAPI()
 
@@ -27,19 +30,62 @@ app = FastAPI()
 #     return templates.TemplateResponse("hello.html", {"request": request})
 
 
+# templates = Jinja2Templates(directory="templates")
+
+# app.mount("/static", StaticFiles(directory="static"),
+# name="static")
+
+# @app.get("/hello/{name}", response_class=HTMLResponse)
+# async def hello(request: Request, name:str):
+#     return templates.TemplateResponse("hello.html", {"request": request, "name": name})
+
 templates = Jinja2Templates(directory="templates")
+# @app.get("/login/", response_class=HTMLResponse)
+# async def login(request: Request):
+#     return templates.TemplateResponse("login.html",{"request": request})
 
-app.mount("/static", StaticFiles(directory="static"),
-name="static")
+# @app.post("/submit")
+# async def submit(request: Request):
+#     form = await request.form()
+#     username = form.get("nm")
+#     password = form.get("pwd")
+#     if username == "madhu" and password == "madhu":
+#         return templates.TemplateResponse("submit.html", {"request": request})
+#     else:
+#         return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/hello/{name}", response_class=HTMLResponse)
-async def hello(request: Request, name:str):
-    return templates.TemplateResponse("hello.html", {"request": request, "name": name})
+# @app.post("/submit")
+# async def submit(nm:str = Form(...),pwd:str = Form(...)):
+#     print(f"Submit called with nm={nm}")
+#     return {"username":nm}
+  
+# @app.get("/upload/", response_class=HTMLResponse)
+# async def upload(request: Request):
+#     return templates.TemplateResponse("uploadfile.html",{"request": request})
+
+
+# @app.post("/upload/")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     with open("destination.png", "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+#     return {"filename": file.filename}
 
 
 
+from fastapi import FastAPI, Cookie
+from fastapi.responses import JSONResponse
+app = FastAPI()
+@app.post("/cookie/")
+def create_cookie():
+    content = {"message": "cookie set"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key="username", value="admin")
+    return response
 
 
+@app.get("/readcookie/")
+async def read_cookie(username: str = Cookie(None)):
+    return {"username": username}
 
 
 
