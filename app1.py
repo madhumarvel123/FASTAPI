0aa040251f44493ddc4ba9906e9a4ff569f0fa68
madhumarvel123 <madhukumargopal1999@gmail.com>
2026-02-28 13:43:09 -0600

    feat: Implement a book management API with CRUD operations using FastAPI and Pydantic.

--- app1.py
+++ app1.py
@@ -6,8 +6,9 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 import shutil   
 from fastapi import Form
-
-
+from typing import List
+from pydantic import BaseModel, Field
+from typing import Tuple
 app = FastAPI()
 
 # @app.get("/hello/", response_class=HTMLResponse)
@@ -74,18 +75,104 @@ templates = Jinja2Templates(directory="templates")
 
 from fastapi import FastAPI, Cookie
 from fastapi.responses import JSONResponse
+# app = FastAPI()
+# @app.post("/cookie/")
+# def create_cookie():
+#     content = {"message": "cookie set"}
+#     response = JSONResponse(content=content)
+#     response.set_cookie(key="username", value="admin", max_age=20, httponly=True, secure=True)
+#     return response
+
+
+# @app.get("/readcookie/")
+# async def read_cookie(username: str = Cookie(None)):
+#     return {"username": username}
+
+# app = FastAPI()
+# class student(BaseModel):
+#     id: int
+#     name :str = Field(None, title="name of student", max_length=10)
+#     marks: List[int] = []
+#     percent_marks: float
+# class percent(BaseModel):
+#     id:int
+#     name :str = Field(None, title="name of student", max_length=10)
+#     percent_marks: float
+# @app.post("/marks", response_model=percent)
+# async def get_percent(s1:student):
+#     s1.percent_marks=sum(s1.marks)/2
+    # return s1
+
+# app = FastAPI()
+# @app.get("/rspheader/")
+# def set_rsp_headers():
+#     content = {"message": "Hello World"}
+#     headers = {"X-Web-Framework": "FastAPI", "Content-Language": "en-US"}
+#     return JSONResponse(content=content, headers=headers)
+
+# app = FastAPI()
+# class supplier(BaseModel):
+#     supplierID:int
+#     supplierName:str
+# class product(BaseModel):
+#     productID:int
+#     prodname:str
+#     price:int
+#     supp:supplier
+# class customer(BaseModel):
+#     custID:int
+#     custname:str
+#     prod:Tuple[product]
+
+# @app.post('/invoice')
+# async def getInvoice(c1:customer):
+#     return c1   
+
+
 app = FastAPI()
-@app.post("/cookie/")
-def create_cookie():
-    content = {"message": "cookie set"}
-    response = JSONResponse(content=content)
-    response.set_cookie(key="username", value="admin", max_age=20, httponly=True, secure=True)
-    return response
+data = []
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str  
 
 
-@app.get("/readcookie/")
-async def read_cookie(username: str = Cookie(None)):
-    return {"username": username}
+@app.post("/book")
+def add_book(book: Book):
+    data.append(book.dict())
+    return data
+
+
+@app.get("/list")
+def get_books():
+    return data 
+
+@app.get("/book/{id}")
+def get_book(id: int):
+    for book in data:
+        if book["id"] == id:
+            return book
+    return {"message": "Book not found"} 
+
+
+@app.put("/book/{id}")
+def update_book(id: int, book: Book):
+    for index, existing_book in enumerate(data):
+        if existing_book["id"] == id:
+            data[index] = book.dict()
+            return data[index]
+
+@app.delete("/book/{id}", status_code=204)
+def delete_book(id: int):
+    for index, book in enumerate(data):
+        if book["id"] == id:
+            data.pop(index)
+            return
+
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
 
 
 
